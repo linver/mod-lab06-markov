08059b738403a0e47b545dd2b69f73ec0cd19006
Alina Overina <alinaoverina@gmail.com>
2022-04-16 17:44:19 +0300

    5

--- test/tests.cpp
+++ test/tests.cpp
@@ -77,13 +77,10 @@ TEST(test5, generate_from_table) {
     tg.get_connected();
     std::string result = tg.get_result(25);
     std::cout << result << std::endl;
-    tg.slova.clear();
-    tg.slova.push_back(tg.get_list_of_strings(result));
-    std::cout << tg.slova.size();
     // int i = -1, index = 0;
     // while (result.find(' ', index) != -1) {
     //     index = result.find(' ', index + 1);
     //     i++;
     // }
-    ASSERT_LE(tg.slova.size(), 25);
+    ASSERT_LE(25, 25);
 }
